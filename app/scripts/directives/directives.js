0ef3ff1fc076dcf22eeae0262b8d169ef87b3261
mario <mario@k15t.com>
2014-12-01 23:16:49 +0100

    small fix to not working directive

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -7,25 +7,4 @@ angular.module('swFrontApp')
 			templateUrl: 'views/task.html',
 			controller: 'TaskCtrl'
 		}
-	})
-  .directive('onlyDigits', function () {
-  return {
-    require: 'ngModel',
-    restrict: 'A',
-    link: function (scope, element, attr, ctrl) {
-      function inputValue(val) {
-        if (val) {
-          var digits = val.replace(/[^0-9.]/g, '');
-
-          if (digits !== val) {
-            ctrl.$setViewValue(digits);
-            ctrl.$render();
-          }
-          return parseFloat(digits);
-        }
-        return undefined;
-      }
-      ctrl.$parsers.push(inputValue);
-    }
-  }
   });
