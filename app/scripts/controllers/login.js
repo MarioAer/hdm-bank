403c1007d067093748a7f57a2d7f7f5f9fc474ba
mario <mario@k15t.com>
2015-01-06 18:02:59 +0100

    hide navbar in login

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,20 +3,16 @@
 angular.module('swFrontApp')
   .controller('LoginCtrl', function ($scope, $location) {
 
-    $scope.check = true;
+
+    angular.element('#navbar').hide();
 
     $scope.go = function ( path ) {
       $location.path( path );
     };
 
     $scope.login = function () {
-      if ($scope.loginForm.$valid) {
-        console.log('sending request to server');
-      } else {
-        $scope.loginForm.submitted = true;
-      }
-
-
-
+      $scope.go('/main');
+      angular.element('#navbar').show();
     }
+
   });
